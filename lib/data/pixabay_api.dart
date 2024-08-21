02c3969bd721ccd564de5ba1ac3029166fd62178
yeongwoo <jyy0223@naver.com>
2024-08-21 18:10:12 +0900

    [feat] mockito 를 이용한 목업 테스트 구현

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -7,12 +7,19 @@ import 'package:image_search/my_settings.dart';
 import '../model/photo.dart';
 
 class PixabayApi implements PhotoApiRepository {
-  final baseUrl = 'https://pixabay.com/api/';
+  static const  baseUrl = 'https://pixabay.com/api/';
 
-  Future<List<Photo>> fetch(String query) async {
+  @override
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
     final uri = Uri.parse(
         "$baseUrl?key=$pixabayKey&q=$query&image_type=photo&pretty=true");
-    final response = await http.get(uri);
+
+    // if (client == null) {
+    //   client = http.Client();
+    // }
+    client ??= http.Client();
+
+    final response = await client.get(uri);
 
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     Iterable hits = jsonResponse['hits'];
